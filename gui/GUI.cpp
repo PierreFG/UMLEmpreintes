0336478b1ddf6a023cd4e968e1e91fff500619a7
lohl <lohl@IF501-210-15>
2018-05-04 15:42:13 +0200

    pleins de nouvelles méthode pour gui (gué ? guuuu ? Grrrr, le film)

---
+++ gui/GUI.cpp
@@ -0,0 +1,82 @@
+/**
+* GUI.h
+* Auteur : Louis Ohl
+* Projet : analyseur d'empreintes (TP GLUML)
+* Date : 4 Mai 2018
+*/
+
+//todo : créer manipulateur de couleur et formattage de texte paramétrable pour meilleure esthétique (classe)
+
+#include <iostream>
+
+void GUI::intro(){
+	cout << "\033[031m"<<endl;
+	cout << "Analyseur d'empreintes" << endl;
+	cout << "Application développée dans un contexte de TP \"Génie logiciel\" à l'INSA de Lyon"<<endl
+	cout << "Copyrights : Pierre Faure--Giovagnoli, Romain Fournier, Alexis Le Conte, Louis Ohl"<<endl<<endl;
+	cout <<"\033[0m";
+}
+
+void GUI::connectionMenu(){
+	cout << "Bienvenue sur l'analyseur d'empreintes !"<<endl<<endl;
+	
+	bool notConnected=true
+	cout<< "Possédez-vous un compte (c) ou voulez-vous vous inscrire (i) ?"<<endl;
+	cout<<"Pour quitter l'application, tapez (q)"<<endl;
+	char res=inputChar();
+	while (res!='i' && res !='c' && res!='q'){
+		cout << "Cela n'était pas demandé ! Recommencez. " << endl;
+		cout << "Possédez-vous un compte (c) ou voulez-vous vous inscrire (i) ?"<<endl;
+		res=inputChar();
+	}
+	if (res=='i'){
+		
+	}
+	if (res=='c'){
+		
+	}
+	if (res=='q'){
+		notConnected=false;
+	}
+}
+
+void GUI::mainMenu(){
+	
+}
+
+int GUI::inputInt(){
+	
+}
+
+string GUI::inputString(){
+	
+}
+
+string GUI::inputPassword(){
+	
+}
+
+char GUI::inputChar(){
+	string resultat;
+	getline(cin, resultat);
+	if (resultat==""){
+		return '\0';
+	} else {
+		return resultat[0];
+	}
+}
+
+Doctor GUI::saisirInformation(){
+	cout <<"Formulaire d'inscription"<<endl<<endl;
+	bool ok=false;
+	do{
+		cout <<"Veuillez rentrer votre prenom :"<<endl;
+		string prenom=inputString();
+		cout << "Veuillez rentrer votre nom de famille :"<<endl;
+		string nom=inputString();
+		cout <<"Veuillez saisir votre email (il vous servira d'identifiant) :"<<endl;
+		string email=inputString();
+		cout <<"Veuillez saisir votre mot de passe :" << endl;
+	
+	} while(!ok);
+}
--- gui/GUI.h
+++ gui/GUI.h
@@ -1,21 +1,29 @@
 /**
-* IHM.h
+* GUI.h
 * Auteur : Louis Ohl
 * Projet : analyseur d'empreintes (TP GLUML)
 * Date : 4 Mai 2018
 */
 
-#ifndef IHM_H
-#define IHM_H
+#ifndef GUI_H
+#define GUI_H
 
-class IHM{
+#include <iostream>
+#include "Doctor.h"
+
+class GUI{
 	public:
 		void intro();
 		void connectionMenu();
 		void mainMenu();
 		int inputInt();
 		string inputString();
+		string inputPassword();
 		char inputChar();
+		
+	private:
+		Doctor saisirInformation();
+		
 }
 
-#endif // IHM_H
+#endif // GUI_H
