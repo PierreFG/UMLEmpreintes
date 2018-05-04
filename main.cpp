9688b6f243a69a97431660de4a051898f7b0154f
AlexisLeConte <alexis.leconte@outlook.fr>
2018-05-04 15:33:14 +0200

    hgfcgfhv

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include "./modele/doctor.h"
 
+using namespace std;
+
 int main(){
     Doctor * d = new Doctor("Louis", "Ohl", "louis.ohl@ina-lyon.fr", "a");
     cout << *d << endl;
     return 0;
 }
+
+
