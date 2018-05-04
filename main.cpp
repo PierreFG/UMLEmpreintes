2b6c3ab88e6d2ea4f113d546b985cad9997d9425
AlexisLeConte <alexis.leconte@outlook.fr>
2018-05-04 15:39:10 +0200

    Revert "hgfcgfhv"
    This reverts commit 9688b6f243a69a97431660de4a051898f7b0154f.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
 #include "./modele/doctor.h"
 
-using namespace std;
-
 int main(){
     Doctor * d = new Doctor("Louis", "Ohl", "louis.ohl@ina-lyon.fr", "a");
     cout << *d << endl;
     return 0;
 }
-
-
