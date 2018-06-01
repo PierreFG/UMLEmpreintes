883a8fe774b5b748a74ca9a818701a8ea075387c
PierreFG <pierre.faure.giovagnoli@gmail.com>
2018-06-01 16:25:29 +0200

    plein de choses

--- tests/unit tests/Unitaire.cpp
+++ tests/unit tests/Unitaire.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 
 #include <set>
+#include <map>
+#include <vector>
+
 
 #include "fs/FileServices.h"
 #include "ui/UI.h"
 #include "model/doctor.h"
 #include "analysis/Vect.h"
 #include "analysis/Mat.h"
+#include "model/rule.h"
 
 using namespace std;
 using namespace ui;
+using namespace fs;
 
 void test1(){
 	//Vérification de la saisie correcte d'un entier
@@ -207,6 +212,20 @@ void testD9(){
     M.set(2,2,12);
     M.afficher();
 
+}
+
+void testC2(){
+    map<string,vector<double>> map1;
+	vector<double> v1; v1.push_back(5.0); v1.push_back(4.0);
+	map1["rhume"] = v1;
+	vector<double> v2; v2.push_back(5.0); v2.push_back(2.0);
+	map1["cancer"] = v2;
+    vector<double> v3;
+	map1["varicelle"] = v3;
+	Rule_ptr ru = make_shared<Rule>(map1);
+
+	Rule_ptr r = fs::getRule();
+	cout << *r;
 }
 
 int main() {
